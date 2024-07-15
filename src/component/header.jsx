6217f5d58a008335c0f7c460f37ee2b6135d5f47
jelly <32786620+puttimeth@users.noreply.github.com>
2024-07-15 10:30:33 +0700

    add message when copy link

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,5 +1,5 @@
 import "./header.scss";
-import { Button } from "antd";
+import { Button, message } from "antd";
 import React from "react";
 import { deckStateToBase64 } from "utils";
 
@@ -7,6 +7,7 @@ export const Header = ({ deckState }) => {
   const copyShareLinkToClipBoard = () => {
     const base64String = deckStateToBase64(deckState);
     navigator.clipboard.writeText("localhost:3000/" + base64String);
+    message.info("Shareable link is copied to clipboard.");
   };
 
   return (
