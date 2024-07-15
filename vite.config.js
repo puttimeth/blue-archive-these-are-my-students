53d6298f225ce791b776bdc388e43688b7eff446
puttimeth <puttimeth.suppawawisit@gmail.com>
2024-07-15 16:37:53 +0700

    update url path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/these-are-my-students/",
+  base: "/blue-archive-these-are-my-students/",
   css: {
     modules: {
       localsConvention: "camelCaseOnly",
