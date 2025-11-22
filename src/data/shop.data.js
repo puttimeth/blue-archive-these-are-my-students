afd167ca5319f6f796ef03c3e67f19b034ea0efe
puttimeth <puttimeth.suppawawisit@gmail.com>
2025-11-22 10:53:58 +0700

    [data] update grand raid boss for jp and global

--- src/data/shop.data.js
+++ src/data/shop.data.js
@@ -3,7 +3,7 @@ export const shopDataJp = {
   // prettier-ignore
   "Raid boss": new Set(["10007", "10016", "10019", "10020", "13000", "13011", "20009", "20013", "23002", "23007", "26000", "26001"]),
   // prettier-ignore
-  "Grand Raid boss": new Set(["10018", "10024", "10028", "10029", "10034", "20011", "20012", "20015"]),
+  "Grand Raid boss": new Set(["10018", "10024", "10028", "10029", "10034", "10043", "20011", "20012", "20015"]),
   // prettier-ignore
   "Arena": new Set(["10038", "20002", "20003", "23001", "23004", "23006"]),
   // prettier-ignore
@@ -14,7 +14,7 @@ export const shopDataGlobal = {
   // prettier-ignore
   "Raid boss": new Set(["10007", "10016", "10019", "10020", "13000", "13011", "20009", "20013", "23002", "23007", "26000", "26001"]),
   // prettier-ignore
-  "Grand Raid boss": new Set(["10018", "10028", "10029", "10034", "20011", "20012", "20015"]),
+  "Grand Raid boss": new Set(["10018", "10024", "10028", "10029", "10034", "20011", "20012", "20015"]),
   // prettier-ignore
   "Arena": new Set(["10038", "20002", "20003", "23001", "23004", "23006"]),
   // prettier-ignore
